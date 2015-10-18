1d0a4d5a0068657caa1f4122a72af4c1a556c103
juancarlospaco <juancarlospaco@gmail.com>
2015-10-18 16:17:07 -0300

    Clean out

--- dookumentation.py
+++ dookumentation.py
@@ -78,7 +78,7 @@ os.environ["PYTHONIOENCODING"], sys.dont_write_bytecode = "utf-8", True
 # Stealth Strings
 
 
-def stealth_to_string(stringy: str, rot13: bool=False) -> str:
+def stealth_to_string(stringy: str) -> str:
     """Stealth to string,stealth is a hidden string,both str type and ttf-8."""
     def __i2b(integ):  # int to bytes, do not touch.
         """Helper for string_to_stealth and stealth_to_string, dont touch!."""
@@ -86,7 +86,6 @@ def stealth_to_string(stringy: str, rot13: bool=False) -> str:
         return binascii.unhexlify(str("%x" % integ).zfill(__num + (__num & 1)))
     _n = int(str(stringy).replace(u"\u200B", "0").replace(u"\uFEFF", "1"), 2)
     stringy = zlib.decompress(base64.b64decode(__i2b(_n))).decode('utf-8')
-    stringy = codecs.decode(stringy, "rot-13") if rot13 and codecs else stringy
     return str(stringy).strip()
 
 
