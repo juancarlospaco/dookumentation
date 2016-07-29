e109bcd8f3831266eb0c06ece18094cb28bc735a
juancarlospaco <juancarlospaco@gmail.com>
2016-07-29 15:07:18 -0300

    Minor, improve example script

--- example.sh
+++ example.sh
@@ -2,4 +2,4 @@
 # -*- coding: utf-8 -*-
 
 
-python3 ./dookumentation.py --zip --ebook --xml --odt --beep --notify ./dookumentation.py
+python3 ./dookumentation.py --zip --ebook --xml --odt --beep --notify --after date --before date ./dookumentation.py
